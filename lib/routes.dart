aa5ebf4e8e3e8e1f96eab6ce0813c663f3738cbd
Haseeb Azhar <haseebazhar@Haseebs-MacBook-Pro.local>
2024-10-03 00:21:42 +0500

    auth setup

--- lib/routes.dart
+++ lib/routes.dart
@@ -8,32 +8,43 @@ class GenerateRoutes {
   static onGenerateRoute(RouteSettings settings) {
     switch (settings.name) {
       case '/':
-        return PageRouteBuilder(
-          pageBuilder: (_, animation, secondaryAnimation) =>
-              const WelcomeScreen(),
-          transitionDuration: const Duration(milliseconds: 600),
-        );
+        return _buildPageRoute(const WelcomeScreen());
       case '/signin':
-        return PageRouteBuilder(
-          pageBuilder: (_, animation, secondaryAnimation) =>
-              const SigninScreen(),
-          transitionDuration: const Duration(milliseconds: 600),
-        );
+        return _buildPageRoute(const SigninScreen());
       case '/brand-agency-signup':
-        return PageRouteBuilder(
-          pageBuilder: (_, animation, secondaryAnimation) =>
-              const BrandAgencyScreen(),
-          transitionDuration: const Duration(milliseconds: 600),
-        );
+        return _buildPageRoute(const BrandAgencyScreen());
       case '/creator-signup':
-        return PageRouteBuilder(
-          pageBuilder: (_, animation, secondaryAnimation) =>
-              const CreatorScreen(),
-          transitionDuration: const Duration(milliseconds: 600),
-        );
-
+        return _buildPageRoute(const CreatorScreen());
       default:
         return MaterialPageRoute(builder: (_) => const WelcomeScreen());
     }
+  }
+
+  // A helper method to build page routes with a consistent transition
+  static PageRouteBuilder _buildPageRoute(Widget page) {
+    return PageRouteBuilder(
+      pageBuilder: (context, animation, secondaryAnimation) => page,
+      transitionDuration: const Duration(
+          milliseconds: 400), // Quick but smooth transition duration
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        // Define a fade transition with a slight upward slide for a smooth feel
+        const begin = Offset(0.0, 0.1); // Start a bit lower
+        const end = Offset.zero; // End at normal position
+        const curve = Curves.easeInOut;
+
+        var tween =
+            Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+        var fadeTween =
+            Tween<double>(begin: 0.0, end: 1.0).chain(CurveTween(curve: curve));
+
+        return FadeTransition(
+          opacity: fadeTween.animate(animation),
+          child: SlideTransition(
+            position: tween.animate(animation),
+            child: child,
+          ),
+        );
+      },
+    );
   }
 }
