d50e5bf2935652100d9202a73427dfdc0199eb32
Haseeb Azhar <haseebazhar@Haseebs-MacBook-Pro.local>
2024-10-05 14:28:14 +0500

    feat(signup): add signup bloc

--- lib/features/auth/application/signup/signup_event.dart
+++ lib/features/auth/application/signup/signup_event.dart
@@ -1,4 +1,36 @@
 part of 'signup_bloc.dart';
 
+class SignupBrandSubmitted extends SignupEvent {
+  final String firstName;
+  final String lastName;
+  final String email;
+  final String? website;
+  final String password;
+  final String accountType;
+
+  SignupBrandSubmitted({
+    required this.firstName,
+    required this.lastName,
+    required this.email,
+    this.website,
+    required this.password,
+    required this.accountType,
+  });
+}
+
 @immutable
 sealed class SignupEvent {}
+
+class SignupInfluencerSubmitted extends SignupEvent {
+  final String firstName;
+  final String lastName;
+  final String email;
+  final String password;
+
+  SignupInfluencerSubmitted({
+    required this.firstName,
+    required this.lastName,
+    required this.email,
+    required this.password,
+  });
+}
--- lib/features/auth/application/signup/signup_state.dart
+++ lib/features/auth/application/signup/signup_state.dart
@@ -1,6 +1,16 @@
 part of 'signup_bloc.dart';
 
+final class SignupFailure extends SignupState {
+  final String error;
+
+  SignupFailure(this.error);
+}
+
 final class SignupInitial extends SignupState {}
+
+final class SignupLoading extends SignupState {}
 
 @immutable
 sealed class SignupState {}
+
+final class SignupSuccess extends SignupState {}
