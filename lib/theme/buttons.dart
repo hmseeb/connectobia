7c63dd27930c0a82fc898064227c2169b8afe530
Haseeb Azhar <haseebazhar@Haseebs-MacBook-Pro.local>
2024-10-05 14:29:04 +0500

    refactor: add circular progress indicator on loading

--- lib/theme/buttons.dart
+++ lib/theme/buttons.dart
@@ -3,17 +3,33 @@ import 'package:shadcn_ui/shadcn_ui.dart';
 
 class PrimaryAuthButton extends StatelessWidget {
   final String text;
+  final bool isLoading;
   final void Function() onPressed;
   const PrimaryAuthButton(
-      {super.key, required this.text, required this.onPressed});
+      {super.key,
+      required this.text,
+      required this.onPressed,
+      required this.isLoading});
 
   @override
   Widget build(BuildContext context) {
+    // get shad theme data
+    final shadTheme = ShadTheme.of(context);
     return SizedBox(
       width: 350,
       child: ShadButton(
         onPressed: onPressed,
-        child: Text(text),
+        child: isLoading
+            ? SizedBox(
+                height: 20,
+                width: 20,
+                child: CircularProgressIndicator(
+                  color: shadTheme.brightness == Brightness.dark
+                      ? Colors.black
+                      : Colors.white,
+                ),
+              )
+            : Text(text),
       ),
     );
   }
