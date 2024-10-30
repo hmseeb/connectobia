2cdc1b8dc0872a34caf21e38cec8d7f12ce5bf6d
Haseeb Azhar <haseebazhar@Haseebs-MacBook-Pro.local>
2024-10-30 19:23:18 +0500

    Seperate logo and title widget

---
+++ lib/modules/auth/presentation/widgets/title_logo.dart
@@ -0,0 +1,20 @@
+import 'package:connectobia/globals/widgets/logo.dart';
+import 'package:connectobia/modules/auth/presentation/widgets/app_title.dart';
+import 'package:flutter/material.dart';
+
+class AppTitleLogo extends StatelessWidget {
+  const AppTitleLogo({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Logo(size: 50),
+        AppTitle('onnectobia'),
+      ],
+    );
+  }
+}
