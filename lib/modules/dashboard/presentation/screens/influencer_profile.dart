cac45085d74312df38675084c5d328df0960dc29
Haseeb Azhar <haseebazhar@Haseebs-MacBook-Pro.local>
2024-12-04 11:11:06 +0500

    Remove image icon

--- lib/modules/dashboard/presentation/screens/influencer_profile.dart
+++ lib/modules/dashboard/presentation/screens/influencer_profile.dart
@@ -5,7 +5,6 @@ import 'package:connectobia/common/extensions/string_extention.dart';
 import 'package:connectobia/common/widgets/transparent_appbar.dart';
 import 'package:connectobia/modules/dashboard/application/influencer_profile/influencer_profile_bloc.dart';
 import 'package:connectobia/modules/dashboard/presentation/views/user_setting.dart';
-import 'package:connectobia/theme/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shadcn_ui/shadcn_ui.dart';
@@ -31,7 +30,6 @@ class _InfluencerProfileState extends State<InfluencerProfile> {
       },
       builder: (context, state) {
         return Scaffold(
-          // message floating action button
           appBar: transparentAppBar(
             state is InfluencerProfileLoaded
                 ? state.influencer.expand.user.username
@@ -47,7 +45,7 @@ class _InfluencerProfileState extends State<InfluencerProfile> {
           floatingActionButton: FloatingActionButton(
             onPressed: () {},
             child: const Icon(
-              Icons.message,
+              LucideIcons.messageSquare,
             ),
           ),
           body: Skeletonizer(
@@ -74,55 +72,25 @@ class _InfluencerProfileState extends State<InfluencerProfile> {
                             fit: BoxFit.cover,
                           ),
                         ),
-                        // Avatar image with camera icon
                         Positioned(
                           bottom: 0,
                           left: 10,
-                          child: GestureDetector(
-                            behavior: HitTestBehavior.translucent,
-                            onTap: () async {
-                              // show cuperino action sheet
-                            },
-                            child: Center(
-                              child: Stack(
-                                alignment: Alignment.bottomRight,
-                                children: [
-                                  CircleAvatar(
-                                    radius: 50,
-                                    backgroundImage: CachedNetworkImageProvider(
-                                      state is InfluencerProfileLoaded
-                                          ? Avatar.getUserImage(
-                                              id: state
-                                                  .influencer.expand.user.id,
-                                              image: state.influencer.expand
-                                                  .user.avatar)
-                                          : Avatar.getAvatarPlaceholder(
-                                              'H', 'A'),
-                                    ),
+                          child: Center(
+                            child: Stack(
+                              alignment: Alignment.bottomRight,
+                              children: [
+                                CircleAvatar(
+                                  radius: 50,
+                                  backgroundImage: CachedNetworkImageProvider(
+                                    state is InfluencerProfileLoaded
+                                        ? Avatar.getUserImage(
+                                            id: state.influencer.expand.user.id,
+                                            image: state
+                                                .influencer.expand.user.avatar)
+                                        : Avatar.getAvatarPlaceholder('H', 'A'),
                                   ),
-                                  Positioned(
-                                    bottom: 0,
-                                    right: 0,
-                                    child: SizedBox(
-                                      height: 25,
-                                      width: 25,
-                                      child: CircleAvatar(
-                                        backgroundColor:
-                                            brightness == Brightness.light
-                                                ? ShadColors.light
-                                                : ShadColors.dark,
-                                        child: Icon(
-                                          Icons.camera_alt,
-                                          color: brightness == Brightness.light
-                                              ? ShadColors.dark
-                                              : ShadColors.light,
-                                          size: 15,
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
+                                ),
+                              ],
                             ),
                           ),
                         ),
