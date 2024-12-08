a3d5e0df353524ce707d81114decded03943f4d7
Haseeb Azhar <haseebazhar@Haseebs-MacBook-Pro.local>
2024-12-08 20:33:37 +0500

    chore: Remove evening from greetings

--- lib/common/constants/greetings.dart
+++ lib/common/constants/greetings.dart
@@ -2,7 +2,6 @@ class Greetings {
   static const String morning = 'Good Morning';
   static const String afternoon = 'Good Afternoon';
   static const String evening = 'Good Evening';
-  static const String night = 'Good Night';
 
   static String getGreeting(String name) {
     final hour = DateTime.now().hour;
@@ -10,10 +9,8 @@ class Greetings {
       return '$morning, $name!';
     } else if (hour < 17) {
       return '$afternoon, $name!';
-    } else if (hour < 20) {
-      return '$evening, $name!';
     } else {
-      return '$night, $name!';
+      return '$evening, $name!';
     }
   }
 }
