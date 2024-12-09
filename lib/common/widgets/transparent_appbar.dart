4c6734664d63a014fe7bb46e1d13617ff0709fbf
Haseeb Azhar <haseebazhar@Haseebs-MacBook-Pro.local>
2024-12-09 15:00:31 +0500

    chore: App bar background color changing default color

--- lib/common/widgets/transparent_appbar.dart
+++ lib/common/widgets/transparent_appbar.dart
@@ -1,6 +1,4 @@
-import 'package:connectobia/theme/colors.dart';
 import 'package:flutter/material.dart';
-import 'package:shadcn_ui/shadcn_ui.dart';
 
 /// A function that returns a transparent app bar
 ///
@@ -11,13 +9,9 @@ import 'package:shadcn_ui/shadcn_ui.dart';
 /// {@category Widgets}
 AppBar transparentAppBar(String? title,
     {List<Widget>? actions, required BuildContext context}) {
-  final shadTheme = ShadTheme.of(context);
   return AppBar(
     elevation: 0,
     title: Text(title ?? ''),
-    backgroundColor: shadTheme.brightness == Brightness.dark
-        ? ShadColors.dark
-        : ShadColors.light,
     actions: actions,
   );
 }
