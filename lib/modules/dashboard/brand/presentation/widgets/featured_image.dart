0dfc66526594b56b291b0df3fb623814b1018d7e
Haseeb Azhar <haseebazhar@Haseebs-MacBook-Pro.local>
2024-12-18 20:37:50 +0500

    chore: Cache user avatar and bannar

--- lib/modules/dashboard/brand/presentation/widgets/featured_image.dart
+++ lib/modules/dashboard/brand/presentation/widgets/featured_image.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:skeletonizer/skeletonizer.dart';
 
@@ -17,20 +18,22 @@ class FeatureImage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return image.isNotEmpty
-        ? Image.network(
-            Avatar.getUserImage(
-                userId: id, image: image, collectionId: collectionId),
-            width: double.infinity,
-            height: 300,
-            fit: BoxFit.cover,
-          )
-        : Skeleton.shade(
-            child: Container(
-              width: double.infinity,
-              height: 300,
-              color: Colors.grey[300],
-            ),
-          );
+    if (image.isNotEmpty) {
+      return CachedNetworkImage(
+        imageUrl: Avatar.getUserImage(
+            userId: id, image: image, collectionId: collectionId),
+        width: double.infinity,
+        height: 300,
+        fit: BoxFit.cover,
+      );
+    } else {
+      return Skeleton.shade(
+        child: Container(
+          width: double.infinity,
+          height: 300,
+          color: Colors.grey[300],
+        ),
+      );
+    }
   }
 }
