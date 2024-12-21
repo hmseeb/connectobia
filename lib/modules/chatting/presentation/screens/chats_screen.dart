0f95c61e8c83e2c094b52fb25bdade2617fe24dd
Akif-Imtiaz <bhiakif@gmail.com>
2024-12-21 18:16:18 +0500

    feat: Implement user interface for chatting

--- lib/modules/chatting/presentation/screens/chats_screen.dart
+++ lib/modules/chatting/presentation/screens/chats_screen.dart
@@ -1,3 +1,4 @@
+import 'package:connectobia/modules/messaging/presentation/widgets/chats_list.dart';
 import 'package:connectobia/shared/presentation/widgets/transparent_appbar.dart';
 import 'package:flutter/material.dart';
 import 'package:shadcn_ui/shadcn_ui.dart';
@@ -32,28 +33,7 @@ class _ChatsState extends State<Chats> {
           IconButton(onPressed: () {}, icon: const Icon(LucideIcons.plus)),
         ],
       ),
-      body: ListView.separated(
-        controller: _scrollController,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: const Text('User Name'),
-            subtitle: const Text('Last message'),
-            leading: const CircleAvatar(
-              child: Text('U'),
-            ),
-            onTap: () {
-              Navigator.of(context, rootNavigator: true)
-                  .pushNamed('/singleChatScreen');
-            },
-            // add trailing icon to show unread messages
-            trailing: const Text('8 min ago'),
-          );
-        },
-        separatorBuilder: (context, index) => Divider(
-          height: 0,
-        ),
-        itemCount: 10,
-      ),
+      body: ChatsList(scrollController: _scrollController),
     );
   }
 
---
+++ lib/modules/chatting/presentation/widgets/message_input.dart
@@ -0,0 +1,39 @@
+import 'package:connectobia/theme/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:shadcn_ui/shadcn_ui.dart';
+
+class MessageInput extends StatelessWidget {
+  const MessageInput({
+    super.key,
+    required TextEditingController messageController,
+  }) : _messageController = messageController;
+
+  final TextEditingController _messageController;
+
+  @override
+  Widget build(BuildContext context) {
+    final Brightness brightness = ShadTheme.of(context).brightness;
+    return Expanded(
+      child: ShadInputFormField(
+        controller: _messageController,
+        placeholder: Text('Enter your message'),
+        decoration: ShadDecoration(
+          secondaryFocusedBorder: ShadBorder.all(
+            width: 2,
+            color: brightness == Brightness.light
+                ? ShadColors.dark
+                : ShadColors.light,
+            radius: BorderRadius.circular(24),
+          ),
+          border: ShadBorder.all(
+            color: Colors.transparent,
+            radius: BorderRadius.circular(24),
+          ),
+          color: brightness == Brightness.dark
+              ? ShadColors.darkForeground
+              : ShadColors.lightForeground,
+        ),
+      ),
+    );
+  }
+}
