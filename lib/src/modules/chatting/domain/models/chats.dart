62020391da1f39733217ebb10a4ebba99142615c
Haseeb Azhar <hsbazr@icloud.com>
2024-12-22 21:18:51 +0500

    feat: Add ability to expand users, messages and influencers

--- lib/src/modules/chatting/domain/models/chats.dart
+++ lib/src/modules/chatting/domain/models/chats.dart
@@ -1,13 +1,12 @@
 import 'dart:convert';
 
-import 'package:connectobia/src/modules/chatting/domain/models/chat.dart';
 import 'package:connectobia/src/modules/chatting/domain/models/message.dart';
 import 'package:connectobia/src/shared/domain/models/brand.dart';
 import 'package:connectobia/src/shared/domain/models/influencer.dart';
 import 'package:pocketbase/pocketbase.dart';
 
 class Chats {
-  final List<Chat> items;
+  final List<Item> items;
   final int page;
   final int perPage;
   final int totalItems;
@@ -21,15 +20,15 @@ class Chats {
     required this.totalPages,
   });
 
-  factory Chats.fromRawJson(String str) => Chats.fromJson(json.decode(str));
-
   factory Chats.fromRecord(ResultList<RecordModel> record) =>
       Chats.fromJson(record.toJson());
+
+  factory Chats.fromRawJson(String str) => Chats.fromJson(json.decode(str));
 
   String toRawJson() => json.encode(toJson());
 
   factory Chats.fromJson(Map<String, dynamic> json) => Chats(
-        items: List<Chat>.from(json["items"].map((x) => Chat.fromJson(x))),
+        items: List<Item>.from(json["items"].map((x) => Item.fromJson(x))),
         page: json["page"],
         perPage: json["perPage"],
         totalItems: json["totalItems"],
@@ -42,6 +41,66 @@ class Chats {
         "perPage": perPage,
         "totalItems": totalItems,
         "totalPages": totalPages,
+      };
+}
+
+class Item {
+  final bool acceptedRequest;
+  final String brand;
+  final String collectionId;
+  final String collectionName;
+  final DateTime created;
+  final bool declinedRequest;
+  final Expand expand;
+  final String id;
+  final String influencer;
+  final String message;
+  final DateTime updated;
+
+  Item({
+    required this.acceptedRequest,
+    required this.brand,
+    required this.collectionId,
+    required this.collectionName,
+    required this.created,
+    required this.declinedRequest,
+    required this.expand,
+    required this.id,
+    required this.influencer,
+    required this.message,
+    required this.updated,
+  });
+
+  factory Item.fromRawJson(String str) => Item.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory Item.fromJson(Map<String, dynamic> json) => Item(
+        acceptedRequest: json["acceptedRequest"],
+        brand: json["brand"],
+        collectionId: json["collectionId"],
+        collectionName: json["collectionName"],
+        created: DateTime.parse(json["created"]),
+        declinedRequest: json["declinedRequest"],
+        expand: Expand.fromJson(json["expand"]),
+        id: json["id"],
+        influencer: json["influencer"],
+        message: json["message"],
+        updated: DateTime.parse(json["updated"]),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "acceptedRequest": acceptedRequest,
+        "brand": brand,
+        "collectionId": collectionId,
+        "collectionName": collectionName,
+        "created": created.toIso8601String(),
+        "declinedRequest": declinedRequest,
+        "expand": expand.toJson(),
+        "id": id,
+        "influencer": influencer,
+        "message": message,
+        "updated": updated.toIso8601String(),
       };
 }
 
