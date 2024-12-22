ab743fadc2245b6e4cae3a793679dc477dcd6470
Akif-Imtiaz <bhiakif@gmail.com>
2024-12-22 21:10:47 +0500

    chore: Remove theme bloc from brand and influencer dashboard

--- lib/src/modules/dashboard/brand/presentation/screens/brand_dashboard.dart
+++ lib/src/modules/dashboard/brand/presentation/screens/brand_dashboard.dart
@@ -1,8 +1,8 @@
+import 'package:connectobia/src/modules/chatting/application/chats/chats_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shadcn_ui/shadcn_ui.dart';
 
-import '../../../../../shared/application/theme/theme_bloc.dart';
 import '../../../../../shared/domain/models/brand.dart';
 import '../../../../auth/presentation/widgets/brand_featured_listing.dart';
 import '../../../../chatting/presentation/screens/chats_screen.dart';
@@ -28,56 +28,58 @@ class _BrandDashboardState extends State<BrandDashboard> {
   int _selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<ThemeBloc, ThemeState>(
-      builder: (context, state) {
-        return Scaffold(
-          endDrawer: CommonDrawer(
-            name: user.brandName,
-            email: user.email,
-            collectionId: user.collectionId,
-            avatar: '',
-            userId: user.id,
-          ),
-          body: IndexedStack(
-            index: _selectedIndex,
-            children: [
-              NestedScrollView(
-                headerSliverBuilder: (context, innerBoxIsScrolled) => [
-                  CommonAppBar(
-                      userName: user.brandName,
-                      searchPlaceholder: 'Search for influencers'),
-                ],
-                body: SingleChildScrollView(
-                  controller: scrollController,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const SectionTitle('Featured Influencers'),
-                        const SizedBox(height: 16),
-                        BrandFeaturedListings(),
-                      ],
-                    ),
-                  ),
+    return Scaffold(
+      endDrawer: CommonDrawer(
+        name: user.brandName,
+        email: user.email,
+        collectionId: user.collectionId,
+        avatar: '',
+        userId: user.id,
+      ),
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: [
+          NestedScrollView(
+            headerSliverBuilder: (context, innerBoxIsScrolled) => [
+              CommonAppBar(
+                  userName: user.brandName,
+                  searchPlaceholder: 'Search for influencers'),
+            ],
+            body: SingleChildScrollView(
+              controller: scrollController,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SectionTitle('Featured Influencers'),
+                    const SizedBox(height: 16),
+                    BrandFeaturedListings(),
+                  ],
                 ),
               ),
-              Placeholder(),
-              Chats(),
-              Placeholder(),
-            ],
+            ),
           ),
-          bottomNavigationBar: buildBottomNavigationBar(
+          Placeholder(),
+          Chats(),
+          Placeholder(),
+        ],
+      ),
+      bottomNavigationBar: BlocBuilder<ChatsBloc, ChatsState>(
+        builder: (context, state) {
+          return buildBottomNavigationBar(
             selectedIndex: _selectedIndex,
             onItemTapped: (index) {
               setState(() {
                 _selectedIndex = index;
               });
+              if (index == 2) {
+                BlocProvider.of<ChatsBloc>(context).add(GetChats());
+              }
             },
-            state: state,
-          ),
-        );
-      },
+          );
+        },
+      ),
     );
   }
 
