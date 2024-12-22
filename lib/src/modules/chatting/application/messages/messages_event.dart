588e1dcd499f17f534560f682fb4827de17e4f41
Akif-Imtiaz <bhiakif@gmail.com>
2024-12-22 21:14:44 +0500

    feat: Add possible message events

--- lib/src/modules/chatting/application/messages/messages_event.dart
+++ lib/src/modules/chatting/application/messages/messages_event.dart
@@ -3,31 +3,29 @@ part of 'messages_bloc.dart';
 @immutable
 sealed class MessagesEvent {}
 
-class GetMessages extends MessagesEvent {
+class GetMessagesByChatId extends MessagesEvent {
   final String chatId;
 
-  GetMessages(this.chatId);
+  GetMessagesByChatId(this.chatId);
 }
 
 class SendMessage extends MessagesEvent {
+  final String recipientId;
+  final String message;
   final String chatId;
-  final String recipientId;
-  final String message;
+  final Messages messages;
 
-  SendMessage(this.chatId, this.message, this.recipientId);
+  SendMessage({
+    required this.message,
+    required this.recipientId,
+    required this.chatId,
+    required this.messages,
+  });
 }
 
-class CreateChat extends MessagesEvent {
-  final String recipientId;
-  final String message;
-
-  CreateChat(this.recipientId, this.message);
-}
-
-class GetMessagesByChatId extends MessagesEvent {
-  GetMessagesByChatId();
-}
-
-class GetChats extends MessagesEvent {
-  GetChats();
+class GetMessagesByUserId extends MessagesEvent {
+  final String userId;
+  GetMessagesByUserId(
+    this.userId,
+  );
 }
