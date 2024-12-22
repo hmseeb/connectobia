582eb859e40768e9f938d8ec53026592ff8a505b
Akif-Imtiaz <bhiakif@gmail.com>
2024-12-22 21:07:20 +0500

    feat: Add date and time format methods (convert to local)

---
+++ lib/src/shared/data/constants/date_and_time.dart
@@ -0,0 +1,38 @@
+class DateAndTime {
+  static String formatDateTimeTo12Hour(DateTime dateTime) {
+    dateTime = dateTime.toLocal();
+    int hour = dateTime.hour;
+    String minute = dateTime.minute.toString().padLeft(2, '0');
+    String period = hour >= 12 ? 'PM' : 'AM';
+
+    // Convert to 12-hour format
+    hour = hour % 12;
+    hour = hour == 0 ? 12 : hour; // Handle midnight (0 hour) as 12 AM
+
+    return '$hour:$minute $period';
+  }
+
+  static String timeAgo(DateTime dateTime) {
+    dateTime = dateTime.toLocal();
+    Duration difference = DateTime.now().difference(dateTime);
+
+    if (difference.inSeconds < 60) {
+      return '${difference.inSeconds} seconds ago';
+    } else if (difference.inMinutes < 60) {
+      return '${difference.inMinutes} minutes ago';
+    } else if (difference.inHours < 24) {
+      return '${difference.inHours} hours ago';
+    } else if (difference.inDays < 7) {
+      return '${difference.inDays} days ago';
+    } else if (difference.inDays < 30) {
+      int weeks = (difference.inDays / 7).floor();
+      return '$weeks weeks ago';
+    } else if (difference.inDays < 365) {
+      int months = (difference.inDays / 30).floor();
+      return '$months months ago';
+    } else {
+      int years = (difference.inDays / 365).floor();
+      return '$years years ago';
+    }
+  }
+}
