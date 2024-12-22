46743f39d4953f730e99ad59487292f02ecb6f3b
Akif-Imtiaz <bhiakif@gmail.com>
2024-12-22 21:15:45 +0500

    Introduce getChatIdByUserId method

--- lib/src/modules/chatting/data/chats_repository.dart
+++ lib/src/modules/chatting/data/chats_repository.dart
@@ -1,11 +1,13 @@
 import 'package:connectobia/src/modules/chatting/data/messages_repository.dart';
 import 'package:connectobia/src/modules/chatting/domain/models/chats.dart';
+import 'package:connectobia/src/modules/chatting/domain/models/message.dart';
 import 'package:connectobia/src/services/storage/pb.dart';
 import 'package:connectobia/src/shared/data/singletons/account_type.dart';
+import 'package:flutter/material.dart';
 import 'package:pocketbase/pocketbase.dart';
 
 class ChatsRepository {
-  Future<RecordModel> createChat(
+  Future<Message> createChat(
       {required String recipientId, required String messageText}) async {
     try {
       final pb = await PocketBaseSingleton.instance;
@@ -22,18 +24,20 @@ class ChatsRepository {
         "brand": brandId,
       };
 
-      final record = await pb.collection('chats').create(body: body);
-      String chatId = record.id;
+      final chatRecord = await pb.collection('chats').create(body: body);
+
+      String chatId = chatRecord.id;
 
       MessagesRepository msgsRepo = MessagesRepository();
 
-      await msgsRepo.sendMessage(
+      final message = await msgsRepo.sendMessage(
         chatId: chatId,
         recipientId: recipientId,
         messageType: 'text',
         messageText: messageText,
       );
-      return record;
+
+      return message;
     } catch (e) {
       throw ClientException;
     }
@@ -53,7 +57,7 @@ class ChatsRepository {
       final resultList = await pb.collection('chats').getList(
             page: 1,
             perPage: 20,
-            filter: '$accountType == $userId',
+            filter: '$accountType = "$userId"',
             expand: 'influencer,brand,message',
             sort: 'created',
           );
@@ -61,6 +65,50 @@ class ChatsRepository {
       Chats chats = Chats.fromRecord(resultList);
 
       return chats;
+    } catch (e) {
+      debugPrint('$e');
+      throw ClientException;
+    }
+  }
+
+  Future<String> getChatID(String recipientId) async {
+    try {
+      final pb = await PocketBaseSingleton.instance;
+      final String selfId = pb.authStore.record!.id;
+      String accountType = CollectionNameSingleton.instance;
+      String influencerId = accountType == 'influencers' ? selfId : recipientId;
+      String brandId = accountType == 'brands' ? selfId : recipientId;
+      final resultList = await pb.collection('chats').getFirstListItem(
+          'influencer = "$influencerId" && brand = "$brandId"');
+      String chatId = resultList.id;
+      if (chatId.isEmpty) {
+        return '';
+      }
+      return chatId;
+    } catch (e) {
+      throw ClientException;
+    }
+  }
+
+  Future<String> getChatIdByUserId(String userId) async {
+    try {
+      final pb = await PocketBaseSingleton.instance;
+      final String selfId = pb.authStore.record!.id;
+
+      String accountType = CollectionNameSingleton.instance;
+
+      String influencerId = accountType == 'influencers' ? selfId : userId;
+      String brandId = accountType == 'brands' ? selfId : userId;
+
+      final resultList = await pb.collection('chats').getFirstListItem(
+          'influencer = "$influencerId" && brand = "$brandId"');
+      String chatId = resultList.id;
+
+      if (chatId.isEmpty) {
+        return '';
+      }
+
+      return chatId;
     } catch (e) {
       throw ClientException;
     }
