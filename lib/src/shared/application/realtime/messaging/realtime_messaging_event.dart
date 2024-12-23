14fd2724f796ed110aaf614290d152b5c45f534d
Haseeb Azhar <hsbazr@icloud.com>
2024-12-24 01:06:11 +0500

    feat: Merge messaging bloc with realtime messaging and introduce realtime messaging with push notifications

--- lib/src/shared/application/realtime/messaging/realtime_messaging_event.dart
+++ lib/src/shared/application/realtime/messaging/realtime_messaging_event.dart
@@ -1,14 +1,41 @@
 part of 'realtime_messaging_bloc.dart';
 
-@immutable
-sealed class RealtimeMessagingEvent {}
-
-class SubscribeMessages extends RealtimeMessagingEvent {
-  SubscribeMessages();
-}
-
 class AddNewMessage extends RealtimeMessagingEvent {
   final Message message;
 
   AddNewMessage(this.message);
+}
+
+class GetMessagesByChatId extends RealtimeMessagingEvent {
+  final String chatId;
+
+  GetMessagesByChatId(this.chatId);
+}
+
+class GetMessagesByUserId extends RealtimeMessagingEvent {
+  final String userId;
+  GetMessagesByUserId(
+    this.userId,
+  );
+}
+
+@immutable
+sealed class RealtimeMessagingEvent {}
+
+class SendMessage extends RealtimeMessagingEvent {
+  final String recipientId;
+  final String message;
+  final String chatId;
+  final Messages messages;
+
+  SendMessage({
+    required this.message,
+    required this.recipientId,
+    required this.chatId,
+    required this.messages,
+  });
+}
+
+class SubscribeMessages extends RealtimeMessagingEvent {
+  SubscribeMessages();
 }
--- lib/src/shared/application/realtime/messaging/realtime_messaging_state.dart
+++ lib/src/shared/application/realtime/messaging/realtime_messaging_state.dart
@@ -1,5 +1,40 @@
 part of 'realtime_messaging_bloc.dart';
 
+final class MessageNotSent extends RealtimeMessagingState {
+  final String message;
+
+  MessageNotSent(this.message);
+}
+
+final class MessageSending extends RealtimeMessagingState {
+  final String message;
+
+  MessageSending(this.message);
+}
+
+final class MessageSent extends RealtimeMessagingState {
+  final Message message;
+
+  MessageSent(this.message);
+}
+
+final class MessagesLoaded extends RealtimeMessagingState {
+  final Messages messages;
+  final String selfId;
+
+  MessagesLoaded(this.messages, this.selfId);
+}
+
+final class MessagesLoading extends RealtimeMessagingState {}
+
+final class MessagesLoadingError extends RealtimeMessagingState {
+  final String message;
+
+  MessagesLoadingError(this.message);
+}
+
+final class NoMessages extends RealtimeMessagingState {}
+
 final class RealtimeMessageReceived extends RealtimeMessagingState {
   final Message message;
   final String avatar;
