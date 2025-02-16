9eed77d321d3b8cbd73e65422f2ffad4c58cd57f
Akif-Imtiaz <bhiakif@gmail.com>
2025-02-17 01:33:26 +0500

    added follower list in seprate widget

---
+++ lib/src/modules/campaign/presentation/widgets/follower_count.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:shadcn_ui/shadcn_ui.dart';
+
+class FollowerCountSelect extends StatelessWidget {
+  final Function(String) onSelected;
+
+  const FollowerCountSelect({super.key, required this.onSelected});
+
+  @override
+  Widget build(BuildContext context) {
+    // Define follower count options
+    const Map<String, String> followerCounts = {
+      '1k-10k': '1k-10k',
+      '10k-50k': '10k-50k',
+      '50k-100k': '50k-100k',
+      '100k-500k': '100k-500k',
+      '500k-1M': '500k-1M',
+    };
+
+    return ShadSelect<String>(
+      placeholder: const Text('Select follower count'),
+      options: followerCounts.entries
+          .map((e) => ShadOption(value: e.key, child: Text(e.value)))
+          .toList(),
+      selectedOptionBuilder: (context, value) {
+        return Text(followerCounts[value]!);
+      },
+      onChanged: (value) {
+        if (value != null) {
+          onSelected(value);
+        }
+      },
+    );
+  }
+}
