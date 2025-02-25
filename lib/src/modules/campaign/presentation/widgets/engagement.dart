96354f7a35886d51e3b8c7e7ed89da68fd7aaa21
Akif-Imtiaz <bhiakif@gmail.com>
2025-02-25 17:58:16 +0500

    reduced name

--- lib/src/modules/campaign/presentation/widgets/engagement.dart
+++ lib/src/modules/campaign/presentation/widgets/engagement.dart
@@ -19,7 +19,7 @@ class EngagementSelect extends StatelessWidget {
       width: 200, // Fixed width
       height: 50, // Fixed height
       child: ShadSelect<String>(
-        placeholder: const Text('Not Selected'),
+        placeholder: const Text('Engagement'),
         options: engagementOptions.entries
             .map((e) => ShadOption(value: e.key, child: Text(e.value)))
             .toList(),
