63dd556fc3d8939f943f58bb5fae238fb7476395
Akif-Imtiaz <bhiakif@gmail.com>
2025-02-25 17:57:12 +0500

    functionality didnt worked so i removed it

--- lib/src/modules/campaign/presentation/widgets/follower_count.dart
+++ lib/src/modules/campaign/presentation/widgets/follower_count.dart
@@ -1,16 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:shadcn_ui/shadcn_ui.dart';
 
-class FollowerCountSelect extends StatefulWidget {
-  final Function(String?) onSelected;
+class FollowerCountSelect extends StatelessWidget {
+  final Function(String) onSelected;
 
   const FollowerCountSelect({super.key, required this.onSelected});
-
-  @override
-  State<FollowerCountSelect> createState() => _FollowerCountSelectState();
-}
-
-class _FollowerCountSelectState extends State<FollowerCountSelect> {
-  String? _selectedValue;
 
   @override
   Widget build(BuildContext context) {
@@ -26,22 +20,18 @@ class _FollowerCountSelectState extends State<FollowerCountSelect> {
     return SizedBox(
       width: 200, // Fixed width
       height: 50,
-      child: DropdownButton<String>(
-        value: _selectedValue, // Track the selected value
-        hint: const Text('Follower'), // Placeholder text
-        items: followerCounts.entries
-            .map((e) => DropdownMenuItem(
-                  value: e.key,
-                  child: Text(e.value),
-                ))
+      child: ShadSelect<String>(
+        placeholder: const Text('Follower'),
+        options: followerCounts.entries
+            .map((e) => ShadOption(value: e.key, child: Text(e.value)))
             .toList(),
+        selectedOptionBuilder: (context, value) {
+          return Text(followerCounts[value]!);
+        },
         onChanged: (value) {
-          setState(() {
-            // Toggle selection: if the selected value is already selected, deselect it
-            _selectedValue = (_selectedValue == value) ? null : value;
-          });
-          // Notify the parent widget about the change
-          widget.onSelected(_selectedValue);
+          if (value != null) {
+            onSelected(value);
+          }
         },
       ),
     );
