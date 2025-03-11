19ee1bf0083eea9e785f6eb9bbc75096d5f8f943
Akif-Imtiaz <bhiakif@gmail.com>
2025-03-11 23:26:39 +0500

    added opecity on shad card

--- lib/src/modules/campaignView/presentation/widgests/infocard.dart
+++ lib/src/modules/campaignView/presentation/widgests/infocard.dart
@@ -4,34 +4,39 @@ import 'package:shadcn_ui/shadcn_ui.dart';
 class InfoCard extends StatelessWidget {
   final String text;
   final Widget? leading;
-  final bool isMultiline; // New property to support multi-line content
+  final bool isMultiline;
 
   const InfoCard({
     super.key,
     required this.text,
     this.leading,
-    this.isMultiline = false, // Default to false
+    this.isMultiline = false,
   });
 
   @override
   Widget build(BuildContext context) {
     return ShadCard(
       padding: const EdgeInsets.all(16.0),
-      child: Row(
-        crossAxisAlignment: isMultiline ? CrossAxisAlignment.start : CrossAxisAlignment.center,
-        children: [
-          if (leading != null) ...[
-            leading!,
-            const SizedBox(width: 10),
-          ],
-          Expanded(
-            child: Text(
-              text,
-              style: ShadTheme.of(context).textTheme.p,
-              softWrap: true,
+      child: Container(
+        color: Colors.white.withOpacity(0.8), // Semi-transparent background
+        borderRadius: BorderRadius.circular(8.0),
+        padding: const EdgeInsets.all(16.0),
+        child: Row(
+          crossAxisAlignment: isMultiline ? CrossAxisAlignment.start : CrossAxisAlignment.center,
+          children: [
+            if (leading != null) ...[
+              leading!,
+              const SizedBox(width: 10),
+            ],
+            Expanded(
+              child: Text(
+                text,
+                style: ShadTheme.of(context).textTheme.p,
+                softWrap: true,
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
