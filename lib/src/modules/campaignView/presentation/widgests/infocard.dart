f5a9d7dd587f80f9d8e8dc3b10d0174d56b0f416
Akif-Imtiaz <bhiakif@gmail.com>
2025-03-11 23:28:08 +0500

    removed opecity

--- lib/src/modules/campaignView/presentation/widgests/infocard.dart
+++ lib/src/modules/campaignView/presentation/widgests/infocard.dart
@@ -4,37 +4,34 @@ import 'package:shadcn_ui/shadcn_ui.dart';
 class InfoCard extends StatelessWidget {
   final String text;
   final Widget? leading;
-  final bool isMultiline;
+  final bool isMultiline; // New property to support multi-line content
 
   const InfoCard({
     super.key,
     required this.text,
     this.leading,
-    this.isMultiline = false,
+    this.isMultiline = false, // Default to false
   });
 
   @override
   Widget build(BuildContext context) {
     return ShadCard(
       padding: const EdgeInsets.all(16.0),
-      child: Container(
-        color: Colors.white.withOpacity(0.8), // Semi-transparent background
-        child: Row(
-          crossAxisAlignment: isMultiline ? CrossAxisAlignment.start : CrossAxisAlignment.center,
-          children: [
-            if (leading != null) ...[
-              leading!,
-              const SizedBox(width: 10),
-            ],
-            Expanded(
-              child: Text(
-                text,
-                style: ShadTheme.of(context).textTheme.p,
-                softWrap: true,
-              ),
-            ),
+      child: Row(
+        crossAxisAlignment: isMultiline ? CrossAxisAlignment.start : CrossAxisAlignment.center,
+        children: [
+          if (leading != null) ...[
+            leading!,
+            const SizedBox(width: 10),
           ],
-        ),
+          Expanded(
+            child: Text(
+              text,
+              style: ShadTheme.of(context).textTheme.p,
+              softWrap: true,
+            ),
+          ),
+        ],
       ),
     );
   }
